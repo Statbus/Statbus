cb68c1cbb5dad931f9c1fecd9c9edcdce129367f
Nick <nick@nfreader.net>
2025-01-19 09:13:14 -0500

    Add secondary DB connection for statbus

---
+++ src/Repository/StatbusRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\QueryBuilder;
+
+class StatbusRepository
+{
+
+    public const ENTITY = null;
+
+    public const TABLE = '';
+    public const ALIAS = '';
+
+    public const COLUMNS = [];
+
+    public const ORDERBY = '';
+
+    public const PER_PAGE = 60;
+
+    public function __construct(
+        protected Connection $statbusConnection,
+    ) {}
+
+    public function qb(): QueryBuilder
+    {
+        return $this->statbusConnection->createQueryBuilder();
+    }
+
+    public function getBaseQuery(): QueryBuilder
+    {
+        $qb = $this->qb();
+        $qb->select(...static::COLUMNS)
+            ->from(static::TABLE, static::ALIAS);
+        if (static::ORDERBY) {
+            $qb->orderBy(static::ORDERBY, 'DESC');
+        }
+        return $qb;
+    }
+
+    public function findOneBy(string $key, int|string $value): mixed
+    {
+        $qb = $this->getBaseQuery();
+        $result = $qb->where(
+            static::ALIAS . '.' . $key . "=" . $qb->createNamedParameter($value)
+        )
+            ->executeQuery()
+            ->fetchAssociative();
+        if (!$result) {
+            return null;
+        }
+        return $this->parseRow($result);
+    }
+
+    public function parseRow(array $result): object
+    {
+        return call_user_func(static::ENTITY . '::new', $result);
+    }
+}

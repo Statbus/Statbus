0e8b53bc9a4174ea4ebd033218822313bac86590
Nick <nick@nfreader.net>
2025-01-21 16:24:12 -0500

    Make IP badge expect a string

--- src/Twig/Components/IPBadge.php
+++ src/Twig/Components/IPBadge.php
@@ -2,11 +2,10 @@
 
 namespace App\Twig\Components;
 
-use IPTools\IP;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
 final class IPBadge
 {
-    public IP $ip;
+    public string $ip;
 }

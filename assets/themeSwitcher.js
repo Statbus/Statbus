4a26a90f91788d633ea9b1df77996f06214371ad
Nick <nick@nfreader.net>
2025-02-02 10:31:26 -0500

    Themeswitcher updates

--- assets/themeSwitcher.js
+++ assets/themeSwitcher.js
@@ -1,12 +1,34 @@
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2024 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
 (() => {
   "use strict";
 
+  const getStoredTheme = () => localStorage.getItem("theme");
+  const setStoredTheme = (theme) => localStorage.setItem("theme", theme);
+
+  const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme();
+    if (storedTheme) {
+      return storedTheme;
+    }
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  };
+
   const setTheme = (theme) => {
-    if (
-      theme === "auto" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      document.documentElement.setAttribute("data-bs-theme", "dark");
+    if (theme === "auto") {
+      document.documentElement.setAttribute(
+        "data-bs-theme",
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light"
+      );
     } else {
       document.documentElement.setAttribute("data-bs-theme", theme);
     }
@@ -64,8 +86,6 @@
         setStoredTheme(theme);
         setTheme(theme);
         showActiveTheme(theme, true);
-        document.querySelector("body").setAttribute("style", null);
-        setBG();
       });
     });
   });

adfc9f83a1104059063ab765ff69ec1127e5298d
Nick <nick@nfreader.net>
2025-02-12 17:17:16 -0500

    TelemetryDB

---
+++ src/Controller/TelemetryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TelemetryRepository;
+use IPTools\IP;
+use IPTools\Network;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_BAN')]
+#[Route('/telemetry')]
+class TelemetryController extends AbstractController
+{
+
+    public function __construct(
+        private TelemetryRepository $telemetryRepository
+    ) {}
+
+    #[Route('', name: 'telemetry')]
+    public function index(Request $request): Response
+    {
+        $ip = $request->get('ip', null);
+        if ($ip === "") {
+            $ip = null;
+        }
+        if ($ip) {
+            if (str_contains($ip, '/')) {
+                $ip = Network::parse($ip);
+            } else {
+                $ip = IP::parse($ip);
+            }
+        }
+        $cid = $request->get('cid');
+        $ckey = $request->get('ckey');
+        if ($ckey === "") {
+            $ckey = null;
+        }
+
+        if ($cid === "") {
+            $cid = null;
+        }
+        $telemetry = null;
+        if ($ckey || $cid || $ip) {
+            $telemetry = $this->telemetryRepository->findTelemetry(
+                ckey: $ckey,
+                ip: $ip,
+                cid: $cid
+            );
+        }
+        $ckeys = [];
+        $cids = [];
+        $ips = [];
+        if ($telemetry) {
+            foreach ($telemetry as &$t) {
+                $t['ip'] = IP::parseLong($t['address'])->__toString();
+                $ckeys[] = $t['ckey'];
+                $cids[] = $t['computer_id'];
+                $ips[] = $t['ip'];
+            }
+        }
+        return $this->render('telemetry/index.html.twig', [
+            'telemetry' => $telemetry,
+            'ckeys' => array_count_values($ckeys),
+            'cids' => array_count_values($cids),
+            'ips' => array_count_values($ips),
+            'ckey' => $ckey,
+            'cid' => $cid,
+            'ip' => $ip?->__toString()
+        ]);
+    }
+}
---
+++ src/Repository/TelemetryRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+use IPTools\IP;
+use IPTools\Network;
+
+class TelemetryRepository extends TGRepository
+{
+
+    public function getBaseQuery(): QueryBuilder
+    {
+        $qb = $this->qb();
+        $qb
+            ->select(
+                'first_round_id',
+                'latest_round_id',
+                'ckey',
+                'address',
+                'computer_id',
+                'telemetry_ckey'
+            )->from('telemetry_connections')
+            ->orderBy('ckey', 'DESC');
+        return $qb;
+    }
+
+    public function findTelemetry(?string $ckey, mixed $ip, ?int $cid): array
+    {
+        $qb = $this->getBaseQuery();
+        if ($ckey) {
+            $qb->orWhere(
+                $qb->expr()->or(
+                    $qb->expr()->like('ckey', ':ckey'),
+                    $qb->expr()->like('telemetry_ckey', ':ckey')
+                )
+            )->setParameter('ckey', $ckey);
+        }
+        if ($cid) {
+            $qb->orWhere('computer_id LIKE :cid')
+                ->setParameter('cid', $cid);
+        }
+        if ($ip) {
+            if ($ip instanceof Network) {
+                $qb->orWhere('address BETWEEN :start AND :end')
+                    ->setParameter('start', (new IP($ip->getFirstIP()))->toLong())
+                    ->setParameter('end', (new IP($ip->getLastIP()))->toLong());
+            } else {
+                $qb->orWhere('address = :ip')
+                    ->setParameter('ip', $ip->toLong());
+            }
+        }
+
+        $result = $qb->executeQuery();
+        return $result->fetchAllAssociative();
+    }
+}

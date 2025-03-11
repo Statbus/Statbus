caa1537922bbd3f529ae32367e8969845aa3a7f3
Nick <nick@nfreader.net>
2025-03-11 10:46:37 -0400

    Fixes threat level determination

--- src/Entity/Round.php
+++ src/Entity/Round.php
@@ -79,25 +79,25 @@ class Round
     {
         if ($this->threat) {
             switch (true) {
-                case ((int) $this->threat == 0):
+                case ((int) $this->threat['threat_level'] == 0):
                     $this->threat['badge'] = ThreatLevel::WHITE_DWARF;
                     break;
-                case ((int) $this->threat < 19):
+                case ((int) $this->threat['threat_level'] < 19):
                     $this->threat['badge'] = ThreatLevel::GREEN_STAR;
                     break;
-                case ((int) $this->threat < 39):
+                case ((int) $this->threat['threat_level'] < 39):
                     $this->threat['badge'] = ThreatLevel::YELLOW_STAR;
                     break;
-                case ((int) $this->threat < 65):
+                case ((int) $this->threat['threat_level'] < 65):
                     $this->threat['badge'] = ThreatLevel::ORANGE_STAR;
                     break;
-                case ((int) $this->threat < 79):
+                case ((int) $this->threat['threat_level'] < 79):
                     $this->threat['badge'] = ThreatLevel::RED_STAR;
                     break;
-                case ((int) $this->threat < 99):
+                case ((int) $this->threat['threat_level'] < 99):
                     $this->threat['badge'] = ThreatLevel::BLACK_ORBIT;
                     break;
-                case ((int) $this->threat > 100):
+                case ((int) $this->threat['threat_level'] > 100):
                     $this->threat['badge'] = ThreatLevel::MIDNIGHT_SUN;
                     break;
             }

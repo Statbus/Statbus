afca7c4f6abf68004af2e0a4b05fc3c142a7c3f8
Nick Farley <nick@nfreader.net>
2025-08-21 19:45:25 -0400

    Attempt to fix broken round pages

--- src/Service/Round/RoundTimelineService.php
+++ src/Service/Round/RoundTimelineService.php
@@ -10,6 +10,52 @@ class RoundTimelineService
     public static function sortStatsIntoTimeline(array $stats): array
     {
         $timeline = [];
+        //Add server init to timeline
+        // $timeline[] = new TimelineEntry(
+        //     timestamp: $stats['round']->getInit(),
+        //     key: TimelineKeys::SERVER_INIT,
+        //     string: sprintf(
+        //         'Round %s initialized at %s',
+        //         $stats['round']->getId(),
+        //         $stats['round']->getInit()->format('Y-m-d H:i:s')
+        //     )
+        // );
+
+        // if ($stats['round']->getStart()) {
+        //     $timeline[] = new TimelineEntry(
+        //         timestamp: $stats['round']->getStart(),
+        //         key: TimelineKeys::ROUND_START,
+        //         string: sprintf(
+        //             'Round %s starts at %s',
+        //             $stats['round']->getId(),
+        //             $stats['round']->getStart()->format('Y-m-d H:i:s')
+        //         )
+        //     );
+        // }
+
+        // if ($stats['round']->getEnd()) {
+        //     $timeline[] = new TimelineEntry(
+        //         timestamp: $stats['round']->getEnd(),
+        //         key: TimelineKeys::ROUND_END,
+        //         string: sprintf(
+        //             'Round %s ends at %s',
+        //             $stats['round']->getId(),
+        //             $stats['round']->getEnd()->format('Y-m-d H:i:s')
+        //         )
+        //     );
+        // }
+        // if ($stats['round']->getShutdown()) {
+        //     $timeline[] = new TimelineEntry(
+        //         timestamp: $stats['round']->getShutdown(),
+        //         key: TimelineKeys::SERVER_SHUTDOWN,
+        //         string: sprintf(
+        //             'Round %s finished shutting down at %s',
+        //             $stats['round']->getId(),
+        //             $stats['round']->getShutdown()->format('Y-m-d H:i:s')
+        //         )
+        //     );
+        // }
+
         if (isset($stats['explosion'])) {
             foreach ($stats['explosion']->getData() as $e) {
                 $timeline[] = new TimelineEntry(
